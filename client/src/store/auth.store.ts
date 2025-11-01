714e74066a4ff5fa0cfee5c9d15ab2563db36dd3
Rohan Pagare <rohanpagare7599@gmail.com>
2025-11-01 12:08:17 +0530

    persist auth state

--- client/src/store/auth.store.ts
+++ client/src/store/auth.store.ts
@@ -1,26 +1,34 @@
 import { STORAGE_CONSTANTS } from "@/utils/constants";
-import {create} from "zustand";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type UserType = {
-    id: string;
-    email: string
+  id: string;
+  email: string;
 } | null;
 
 interface AuthState {
-    user: UserType
-    setAuth: (user: UserType, isLogin: boolean) => void;
-    logout: () => void;
+  user: UserType;
+  setAuth: (user: UserType, isLogin: boolean) => void;
+  logout: () => void;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-    user: null,
-    isUser: true,
-    setAuth: (user: UserType, isLogin: boolean) => {
-        localStorage.setItem(STORAGE_CONSTANTS.isLogin, `${isLogin}`)
-        set({user})
-    },
-    logout: () => {
-        localStorage.removeItem(STORAGE_CONSTANTS.isLogin)
-        set({user: null})
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      isUser: true,
+      setAuth: (user: UserType, isLogin: boolean) => {
+        localStorage.setItem(STORAGE_CONSTANTS.isLogin, `${isLogin}`);
+        set({ user });
+      },
+      logout: () => {
+        localStorage.removeItem(STORAGE_CONSTANTS.isLogin);
+        set({ user: null });
+      },
+    }),
+    {
+      name: "auth-storage",
     }
-}))
+  )
+);
